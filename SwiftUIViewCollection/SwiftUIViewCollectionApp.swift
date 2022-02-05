1bcf90cd230387702fde5ada168fdaa6772ffa58
Erik Dungan <erik.dungan@gmail.com>
2022-02-05 15:47:40 -0800

    Initial Commit

---
+++ SwiftUIViewCollection/SwiftUIViewCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewCollectionApp.swift
+//  SwiftUIViewCollection
+//
+//  Created by Erik Dungan on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
